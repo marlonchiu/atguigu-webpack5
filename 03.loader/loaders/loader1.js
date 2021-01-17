47bcc5c1065342e6d5abd333eeffff9439102424
marlonchiu <Knight1898@outlook.com>
2021-01-17 19:11:26 +0800

    14-webpack从入门到精通-同步&异步loader

--- 03.loader/loaders/loader1.js
+++ 03.loader/loaders/loader1.js
@@ -1,17 +1,18 @@
 // loader本质上是一个函数
 
 // 同步loader
-module.exports = function (content, map, meta) {
-  console.log(111);
-
-  return content;
-}
-
 // module.exports = function (content, map, meta) {
 //   console.log(111);
 
-//   this.callback(null, content, map, meta);
+//   return content;
 // }
+
+// 同步loader
+module.exports = function (content, map, meta) {
+  console.log(111);
+
+  this.callback(null, content, map, meta);
+}
 
 // pitch 方法按照正序执行
 module.exports.pitch = function () {
--- 03.loader/loaders/loader2.js
+++ 03.loader/loaders/loader2.js
@@ -1,10 +1,22 @@
 // loader本质上是一个函数
 
 // 同步loader
+// module.exports = function (content, map, meta) {
+//   console.log(222);
+
+//   return content;
+// }
+
+// 异步loader
 module.exports = function (content, map, meta) {
   console.log(222);
 
-  return content;
+  const callback = this.async();
+
+  // 异步 loader1 过 1s后再输出
+  setTimeout(() => {
+    callback(null, content);
+  }, 1000)
 }
 
 module.exports.pitch = function () {
